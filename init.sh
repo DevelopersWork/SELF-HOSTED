d27e24d488c56ac2ee9ca73c53fddc46198ca60f
Developers@Work <DevelopersWork@users.noreply.github.com>
2024-05-24 14:08:49 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -15,5 +15,10 @@ done
 sudo /bin/bash -c "$SCRIPTS_DIR/01-setup-docker.sh" || { echo "Error running 01-setup-docker.sh"; exit 1; }
 sudo /bin/bash -c "$SCRIPTS_DIR/02-configure-docker.sh" || { echo "Error running 02-configure-docker.sh"; exit 1; }
 
-sudo -u docker /bin/bash -c "$SCRIPTS_DIR/03-setup-portainer.sh" || { echo "Error running 03-setup-portainer.sh"; exit 1; }
-sudo -u docker /bin/bash -c "$SCRIPTS_DIR/04-setup-dockge.sh" || { echo "Error running 04-setup-dockge.sh"; exit 1; }
+# Switch to the docker user for subsequent scripts 
+exec sudo -u docker -g docker bash << EOF
+# Run scripts 03 and 04 as the docker user 
+echo "Running as user: $(whoami)"
+/bin/bash -c "$SCRIPTS_DIR/03-setup-portainer.sh" || { echo "Error running 03-setup-portainer.sh"; exit 1; }
+/bin/bash -c "$SCRIPTS_DIR/04-setup-dockge.sh" || { echo "Error running 04-setup-dockge.sh"; exit 1; }
+EOF
