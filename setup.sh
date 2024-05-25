d2ea6fd4496e0dc584d1458fef54dd7bcdd0b6fe
Developers@Work <DevelopersWork@users.noreply.github.com>
2024-05-25 16:48:26 +0530

    temporary directory setup

--- setup.sh
+++ setup.sh
@@ -7,23 +7,7 @@ ENV_FILE="./.env"
 # Source the environment file to load variables
 source "$ENV_FILE" || { echo "Failed to source environment file."; exit 1; }
 
-# Constants (for clarity and maintainability)
 SCRIPTS_PATH="$(dirname "$(realpath "$0")")/$SCRIPTS_DIR"
-DOCKER_PATH="/home/docker"
-DOCKER_CONTAINER_PATH="$DOCKER_PATH/$DOCKER_CONTAINER_DIR"
-DOCKER_STACKS_PATH="$DOCKER_PATH/$DOCKER_STACKS_DIR"
-DOCKER_STORAGE_PATH="$DOCKER_PATH/$DOCKER_STORAGE_DIR"
-DOCKER_TEMP_PATH="$DOCKER_PATH/$DOCKER_TEMP_DIR"
-
-# Create a temporary env file
-ENV_FILE=$(mktemp)
-echo "DOCKER_USER=$DOCKER_USER" >> "$ENV_FILE"
-echo "DOCKER_GROUP=$DOCKER_GROUP" >> "$ENV_FILE"
-echo "DOCKER_PATH=$DOCKER_PATH" >> "$ENV_FILE"
-echo "DOCKER_CONTAINER_PATH=$DOCKER_CONTAINER_PATH" >> "$ENV_FILE"
-echo "DOCKER_STACKS_PATH=$DOCKER_STACKS_PATH" >> "$ENV_FILE"
-echo "DOCKER_STORAGE_PATH=$DOCKER_STORAGE_PATH" >> "$ENV_FILE"
-echo "DOCKER_TEMP_PATH=$DOCKER_TEMP_PATH" >> "$ENV_FILE"
 
 # Array of scripts to run
 scripts=("01-dependencies.sh" "02-user-setup.sh" "03-storage-setup.sh" "04-setup-portainer.sh" "05-setup-dockge.sh", "06-setup-stacks.sh")
@@ -34,36 +18,28 @@ for script in "${scripts[@]}"; do
 done
 
 # Run scripts 01, 02, and 03 as root
-for script in "${scripts[@]:0:3}"; do  # Run the first two scripts as root
+for script in "${scripts[@]:0:3}"; do  # Run the first three scripts as root
   sudo /bin/bash "$SCRIPTS_PATH/$script" "$ENV_FILE" || { echo "Error running $script"; exit 1; }
   echo "Script $script completed successfully."
 done
 
-# Get the docker user's UID and GID after user creation
-DOCKER_UID=$(id -u "$DOCKER_USER")
-DOCKER_GID=$(id -g "$DOCKER_USER")
-echo "DOCKER_UID=$DOCKER_UID" >> "$ENV_FILE"
-echo "DOCKER_GID=$DOCKER_GID" >> "$ENV_FILE"
-echo "Docker User ID: $DOCKER_UID"
-echo "Docker Group ID: $DOCKER_GID"
+# Create alias for running commands as the docker user
+AS_DOCKER_USER="sudo -u $DOCKER_USER /bin/bash"
 
-# Ensure docker user has read access to the env file
-sudo chown "$DOCKER_USER" "$ENV_FILE" || { 
-  echo "Failed to grant access to environment file for docker user."
-  exit 1
-}
+# Create a temporary directory for the docker user
+TEMP_DIR=$($AS_DOCKER_USER -c "mktemp -d homelab.XXXXXX") || { echo "Failed to create temporary directory for the docker user."; exit 1; }
+# TEMP_ENV_FILE=$($AS_DOCKER_USER -c "mktemp $TEMP_DIR/.env.XXXXXX") # Future reference
 
-# Create a temporary directory for the scripts under the docker user's home
-DOCKER_SCRIPTS_PATH="$DOCKER_TEMP_PATH/$SCRIPTS_DIR"
-sudo rm -r "$DOCKER_SCRIPTS_PATH"
-sudo cp -r "$SCRIPTS_PATH" "$DOCKER_TEMP_PATH/"
-sudo chown -R "$DOCKER_USER":"$DOCKER_GROUP" "$DOCKER_SCRIPTS_PATH"
+# Copy the bash scripts and env file to the temporary directory
+sudo cp -r "$SCRIPTS_PATH" "$ENV_FILE" "$TEMP_DIR/"
+sudo chown -R "$DOCKER_USER":"$DOCKER_GROUP" "$TEMP_DIR"
+echo "Created temporary directory for docker user and Copied scripts and env file to $TEMP_DIR."  # Informational output
 
 # Run scripts 04 and 05 as the 'docker' user
 for script in "${scripts[@]:3}"; do  # Run the remaining scripts as docker user
-  sudo -u "$DOCKER_USER" /bin/bash "$DOCKER_SCRIPTS_PATH/$script" "$ENV_FILE" || { echo "Error running $script"; exit 1; }
+  $AS_DOCKER_USER "$TEMP_DIR/$SCRIPTS_DIR/$script" "$TEMP_DIR/$ENV_FILE" || { echo "Error running $script"; exit 1; }
   echo "Script $script completed successfully."
 done
 
-# Remove the temporary file
-sudo rm "$ENV_FILE"
+# Clean up - remove the temporary directory
+sudo rm -rf "$TEMP_DIR"
