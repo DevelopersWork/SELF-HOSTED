e59f4d7f643eeb936327fd2b13584df118e83312
Developers@Work <DevelopersWork@users.noreply.github.com>
2024-05-27 13:35:55 +0530

    Update deploy.sh

--- stacks/filebrowser/deploy.sh
+++ stacks/filebrowser/deploy.sh
@@ -2,7 +2,7 @@
 # deploy.sh
 
 # Source the utility script
-source "$1/utils.sh" 
+source "$1/utils.sh"
 
 # Get the environment file path and exit if not provided or not a regular file
 # Source the environment file to load variables
@@ -11,10 +11,25 @@ load_config "$2"
 STACK_PATH="$DOCKER_STACKS_PATH/filebrowser"
 
 # .env file
-STACK_ENV_FILE="$STACK_PATH/.env"
-echo "PUID=$(id -u $DOCKER_USER)" > $STACK_ENV_FILE
-echo "PGID=$(id -g $DOCKER_USER)" >> $STACK_ENV_FILE
-echo "FILEBROWSER_VOLUME_PATH=$DOCKER_CONTAINER_PATH/filebrowser" >> $STACK_ENV_FILE
-echo "FILEBROWSER_HTTP_WEBPORT=8082" >> $STACK_ENV_FILE
-echo "FILEBROWSER_RESOURCES_CPUS=0.5" >> $STACK_ENV_FILE
-echo "FILEBROWSER_RESOURCES_MEMORY=512M" >> $STACK_ENV_FILE
+ENV_FILE="$STACK_PATH/.env"
+if [[ ! -f "$ENV_FILE" ]]; then  # Check if .env file exists
+    echo "Creating .env file at $ENV_FILE..."
+    echo "PUID=$(id -u $DOCKER_USER)" > "$ENV_FILE"
+    echo "PGID=$(getent group "$DOCKER_GROUP" | cut -d: -f3)" >> "$ENV_FILE"
+    echo "FILEBROWSER_VOLUME_PATH=$DOCKER_CONTAINER_PATH/filebrowser" >> "$ENV_FILE"
+    echo "FILEBROWSER_HTTP_WEBPORT=8082" >> "$ENV_FILE"
+    echo "FILEBROWSER_RESOURCES_CPUS=0.5" >> "$ENV_FILE"
+    echo "FILEBROWSER_RESOURCES_MEMORY=512M" >> "$ENV_FILE"
+fi
+
+# database.db file
+DATABASE_PATH="$STACK_PATH/database/"
+DATABASE_FILE="$DATABASE_PATH/filebrowser.db"
+create_dir_if_not_exists "$DATABASE_PATH" "$DOCKER_USER" "$DOCKER_GROUP"
+
+# Check if database.db file exists
+if [[ ! -f "$DATABASE_FILE" ]]; then  
+    echo "Creating filebrowser.db file at $DATABASE_FILE..."
+    touch "$DATABASE_FILE"
+    set_ownership "$DATABASE_FILE" "$DOCKER_USER" "$DOCKER_GROUP"
+fi
