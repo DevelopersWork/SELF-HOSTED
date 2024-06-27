eaa6d1a703c543259dbc646c0f3f213e3529386d
Developers@Work <DevelopersWork@users.noreply.github.com>
2024-06-27 23:58:03 +0530

    stack update coolify

--- stacks/coolify/deploy.sh
+++ stacks/coolify/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# stacks/pihole-unbound/deploy.sh
-
-UNBOUND_CONFIG_URL="https://raw.githubusercontent.com/MatthewVance/unbound-docker/master/1.19.3/data/opt/unbound/etc/unbound"
+# stacks/coolify/deploy.sh
 
 # Source the utility script
 source "$1/utils.sh"
@@ -10,44 +8,36 @@ source "$1/utils.sh"
 # Source the environment file to load variables
 load_config "$2"
 
-# Define and Create the Pihole Unbound stack directory
-STACK_PATH="$DOCKER_STACKS_PATH/pihole-unbound"
+# Define and Create the Nginx Proxy Manager stack directory
+STACK_PATH="$DOCKER_STACKS_PATH/coolify"
 create_dir_if_not_exists "$STACK_PATH" "$DOCKER_USER" "$DOCKER_GROUP"
 
-# Define and Create the Pihole and Unbound volume directory
-PIHOLE_VOLUME_PATH="$DOCKER_CONTAINER_PATH/pihole"
-UNBOUND_VOLUME_PATH="$DOCKER_CONTAINER_PATH/unbound"
-create_dir_if_not_exists "$PIHOLE_VOLUME_PATH" "$DOCKER_USER" "$DOCKER_GROUP"
-create_dir_if_not_exists "$UNBOUND_VOLUME_PATH" "$DOCKER_USER" "$DOCKER_GROUP"
+# Define and Create the Nginx Proxy Manager volume directory
+VOLUME_PATH="$DOCKER_CONTAINER_PATH/coolify"
+create_dir_if_not_exists "$VOLUME_PATH" "$DOCKER_USER" "$DOCKER_GROUP"
 
 # .env file
 ENV_FILE="$STACK_PATH/.env"
 create_file_if_not_exists "$ENV_FILE" "$DOCKER_USER" "$DOCKER_GROUP" 
 update_env_file $ENV_FILE "PUID" "$(id -u $DOCKER_USER)"
 update_env_file $ENV_FILE "PGID" "$(getent group $DOCKER_GROUP | cut -d: -f3)"
-update_env_file $ENV_FILE "PIHOLE_DNS_PORT" "53"
-update_env_file $ENV_FILE "PIHOLE_HTTP_WEBPORT" "1010"
-update_env_file $ENV_FILE "PIHOLE_HTTPS_WEBPORT" "4443"
-update_env_file $ENV_FILE "PIHOLE_WEBPASSWORD" "password"
-update_env_file $ENV_FILE "PIHOLE_DNSMASQ_LISTENING" "single" # <local|all|single>
-update_env_file $ENV_FILE "PIHOLE_CACHE_SIZE" "10000"
-update_env_file $ENV_FILE "PIHOLE_VOLUME_PATH" "$PIHOLE_VOLUME_PATH"
-update_env_file $ENV_FILE "PIHOLE_RESOURCES_CPUS" "0.5"
-update_env_file $ENV_FILE "PIHOLE_RESOURCES_MEMORY" "256M"
-update_env_file $ENV_FILE "UNBOUND_VOLUME_PATH" "$UNBOUND_VOLUME_PATH"
-update_env_file $ENV_FILE "UNBOUND_RESOURCES_CPUS" "0.5"
-update_env_file $ENV_FILE "UNBOUND_RESOURCES_MEMORY" "256M"
+update_env_file $ENV_FILE "COOLIFY_HTTP_WEBPORT" "80"
+update_env_file $ENV_FILE "COOLIFY_VOLUME_PATH" "$VOLUME_PATH"
+update_env_file $ENV_FILE "COOLIFY_RESOURCES_CPUS" "0.5"
+update_env_file $ENV_FILE "COOLIFY_RESOURCES_MEMORY" "512M"
+update_env_file $ENV_FILE "POSTGRES_USER" "POSTGRES_USER"
+update_env_file $ENV_FILE "POSTGRES_PASSWORD" "POSTGRES_PASSWORD"
+update_env_file $ENV_FILE "POSTGRES_DB" "POSTGRES_DB"
+update_env_file $ENV_FILE "POSTGRES_RESOURCES_CPUS" "0.5"
+update_env_file $ENV_FILE "POSTGRES_RESOURCES_MEMORY" "256M"
+update_env_file $ENV_FILE "REDIS_PASSWORD" "REDIS_PASSWORD"
+update_env_file $ENV_FILE "REDIS_RESOURCES_CPUS" "0.5"
+update_env_file $ENV_FILE "REDIS_RESOURCES_MEMORY" "256M"
+update_env_file $ENV_FILE "PUSHER_APP_ID" "256M"
+update_env_file $ENV_FILE "PUSHER_APP_KEY" "256M"
+update_env_file $ENV_FILE "PUSHER_APP_SECRET" "256M"
+update_env_file $ENV_FILE "SOKETI_RESOURCES_CPUS" "0.5"
+update_env_file $ENV_FILE "SOKETI_RESOURCES_MEMORY" "256M"
 
-# Download Unbound Configuration Files
-for file in "forward-records.conf" "a-records.conf" "srv-records.conf"; do
-    config_file="$UNBOUND_VOLUME_PATH/$file"
-    if [[ ! -f "$config_file" ]]; then
-        echo "Downloading Unbound configuration file: $file"
-        wget -q -O "$config_file" "$UNBOUND_CONFIG_URL/$file" || {
-            echo "Failed to download Unbound configuration: $file" >&2
-            exit 1
-        }
-        set_ownership "$config_file" "$DOCKER_USER" "$DOCKER_GROUP"
-        set_permissions "$config_file" "644"
-    fi
-done
+create_dir_if_not_exists "$VOLUME_PATH/app" "$DOCKER_USER" "$DOCKER_GROUP"
+create_dir_if_not_exists "$VOLUME_PATH/postgresql" "$DOCKER_USER" "$DOCKER_GROUP"
