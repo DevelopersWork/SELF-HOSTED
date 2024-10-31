e47e238ee1d6fe01d8b63ea82f5e5c5bf1972bdc
Developers@Work <DevelopersWork@users.noreply.github.com>
2024-10-31 18:51:48 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ if [[ -f $ENV_FILE ]]; then
 else
   echo "Warning: $ENV_FILE not found."
   ENV_FILE="./.env_TEMPLATE"
-  source "$ENV_FILE" || {
+  source $ENV_FILE || {
     echo "Error: No environment file found. Exiting." >&2
     exit 1
   }
@@ -30,7 +30,7 @@ sudo cp -r "." "$TEMP_DIR/"
 sudo chown -R "$DOCKER_USER":"$DOCKER_GROUP" "$TEMP_DIR"
 
 # Run scripts 04 and 05 as the 'docker' user
-for script in "${scripts[@]:3:2}"; do  
+for script in "${scripts[@]:0:2}"; do  
   $AS_DOCKER_USER "$TEMP_DIR/$SCRIPTS_DIR/$script" "$TEMP_DIR/$SCRIPTS_DIR" "$TEMP_DIR/$ENV_FILE" || { echo "Error running $script"; exit 1; }
   echo "Script $script completed successfully."
 done
