d2b8637fed57d5744d6eb7968566c2af571e04bd
Developers@Work <DevelopersWork@users.noreply.github.com>
2024-11-02 18:20:00 +0530

    UPDATING DOCKERFILE TO DIFFERNET SOURCE

--- stacks/github-actions-runner/deploy.sh
+++ stacks/github-actions-runner/deploy.sh
@@ -14,12 +14,13 @@ create_dir_if_not_exists "$STACK_PATH" "$DOCKER_USER" "$DOCKER_GROUP"
 # Download the Dockerfile
 DOCKERFILE_SHA="e1fa1fcbc3de1c0b57f6fe2a82f77a8ed3a138b1"
 DOCKERFILE_URL="https://raw.githubusercontent.com/actions/runner/${DOCKERFILE_SHA}/images/Dockerfile"
+DOCKERFILE_URL="https://raw.githubusercontent.com/actions-runner-controller/runner-images/refs/heads/main/Dockerfile"
 DOCKERFILE_OUTPUT_PATH="$STACK_PATH/Dockerfile"
 wget -q -O "$DOCKERFILE_OUTPUT_PATH" "$DOCKERFILE_URL" || {
     echo "Failed to download: $DOCKERFILE_URL" >&2
     exit 1
 }
-echo "$DOCKERFILE_SHA  $DOCKERFILE_OUTPUT_PATH" | shasum -a 256 -c
+# echo "$DOCKERFILE_SHA  $DOCKERFILE_OUTPUT_PATH" | shasum -a 256 -c
 set_ownership "$DOCKERFILE_OUTPUT_PATH" "$DOCKER_USER" "$DOCKER_GROUP"
 set_permissions "$DOCKERFILE_OUTPUT_PATH" "644"
 
